b81138ad075d953eac73523a427a4df75b36e7c6
landyeath <landyeath@gmail.com>
2016-06-28 14:12:39 +0800

    test-aa

--- lan_test/test1.py
+++ lan_test/test1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
 print 'this is a hello'
+xd = pd.DataFrame({'A':[1,21,2,4],'B':['xx','aa','bb','cc']},index=['S','Q','O','T'])
+print xd
+print 'hello'
